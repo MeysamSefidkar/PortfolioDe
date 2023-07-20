bd9beb1717fae3f7408271ad67774a3986deb9b5
Meysam Sefidkar <kwpasefidkar@gmail.com>
2023-07-21 01:22:16 +0330

    Add font in project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import {ThemeProvider, createTheme} from "@mui/material/styles";
 import {Button} from "@mui/material";
+import {HelmetProvider, Helmet} from "react-helmet-async";
 import './App.css';
+
+
+const theme = createTheme({
+    typography: {
+        fontFamily: 'Kavivanar,roboto',
+    }
+})
 
 function App() {
     return (
-        <div className="App">
-            <Button variant="contained">click</Button>
-        </div>
+        <ThemeProvider theme={theme}>
+            <HelmetProvider>
+                <Helmet>
+                    <title>Meysam Sefidkars persönliche Website</title>
+                </Helmet>
+                <div className="App">
+                    <Button variant="contained">click</Button>
+                </div>
+            </HelmetProvider>
+        </ThemeProvider>
     );
 }
 
